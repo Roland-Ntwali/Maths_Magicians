0d2e446917a0031f2e3fe37278eec355aa5d5a25
Roland-Ntwali <rolandntwali@gmail.com>
2023-05-03 01:28:05 +0200

    Create 3 page website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import Calculator from './components/calculator';
-import Qoute from './components/qoute';
+import { Route, Routes } from 'react-router-dom';
+import CalculatorPage from './pages/calculator';
+import Home from './pages/Home';
+import Navbar from './nav';
+import QoutePage from './pages/Qoute';
 
 function App() {
   return (
-    <div className="App">
-      <div className="qoute_wrapper">
-        <Qoute />
-      </div>
 
-      <div className="calculator_wrapper">
-        <Calculator />
-      </div>
+    <div className="App">
+      <>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/calculator" element={<CalculatorPage />} />
+            <Route path="/qoute" element={<QoutePage />} />
+          </Routes>
+        </div>
+      </>
+
     </div>
   );
 }
